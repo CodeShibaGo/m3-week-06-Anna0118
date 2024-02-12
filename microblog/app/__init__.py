98f5e17aac896abf257a1489cf09886a1820e3d4
Anna0118 <annachu0988@gmail.com>
2024-02-12 15:24:12 +0800

    feat: import flask-wtf

--- microblog/app/__init__.py
+++ microblog/app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from config import Config
 from dotenv import load_dotenv
 import os
 
@@ -10,6 +11,8 @@ database_url = os.getenv('DATABASE_URL')
 secret_key = os.getenv('SECRET_KEY')
 
 app = Flask(__name__)
+# 防止CSRF攻擊
+app.config.from_object(Config)
 
 from app import routes
 
--- microblog/app/routes.py
+++ microblog/app/routes.py
@@ -1,15 +1,11 @@
 from app import app
 from flask import render_template
+from app.forms import LoginForm
 
 
 @app.route('/')
 def home():
     return 'Hello world'
-
-
-@app.route('/about')
-def about():
-    return '<h1>About</h1>'
 
 
 @app.route('/index')
@@ -18,14 +14,24 @@ def index():
     posts = [
         {
             'author': {'username': 'John'},
-            'body': 'Portland 的天氣真好！'
+            'body': 'Beautiful day in Portland!'
         },
         {
             'author': {'username': 'Susan'},
-            'body': '復仇者聯盟電影真的很酷！'
+            'body': 'The Avengers movie was so cool!'
         }
     ]
-    return render_template('index.html', title='首頁', user=user, posts=posts)
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/login')
+def login():
+    form = LoginForm()
+    # if form.validate_on_submit():
+    #     flash('Login requested for user {}, remember_me={}'.format(
+    #         form.username.data, form.remember_me.data))
+    #     return redirect(url_for('index'))
+    return render_template('login.html',  title='Sign In', form=form)
 
 if __name__ == '__main__':
     app.run()
