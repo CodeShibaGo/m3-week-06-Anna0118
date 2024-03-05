ddc5e35dbfe83dd7843cd45b5c56f55953845c3c
Anna0118 <annachu0988@gmail.com>
2024-03-05 23:11:48 +0800

    feat: add error handling

--- microblog/config.py
+++ microblog/config.py
@@ -3,3 +3,9 @@ import os
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY') or 'you-will-never-guess'
     SQLALCHEMY_DATABASE_URI = os.getenv('DATABASE_URL')
+    MAIL_SERVER = os.environ.get('MAIL_SERVER')
+    MAIL_PORT = int(os.environ.get('MAIL_PORT') or 25)
+    MAIL_USE_TLS = os.environ.get('MAIL_USE_TLS') is not None
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    ADMINS = ['amy3129252@gmail.com']
